03bda3d8773f0c7f98bafbf52b50755a843c5350
Rex Bergeron <rexbergeron@comcast.net>
2020-03-02 11:55:57 -0800

    suri

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const cors = require('cors');
 
 
 
-const getVodkaDrinks = async () => {
-    const drinkApi = await request.get(`https://www.thecocktaildb.com/api/json/v2/${API_KEY}/filter.php?i=vodka}`);
+const getVodkaDrinks = async() => {
+    const drinkApi = await request.get(`https://www.thecocktaildb.com/api/json/v2/${process.env.API_KEY}/filter.php?i=vodka}`);
     return drinkApi.body.drinks.map(vodkaDrink => {
         
         return {
@@ -26,4 +26,10 @@ app.get('/vodka', async(req, res, next) => {
         next(err);   
     }
 
+});
+//made a const called port and go to the port that we made in env OR (||) go to 5000
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log('listening on port', port);
 });
