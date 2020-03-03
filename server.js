9e6595daee0f531b85e93965110f8745069a3ecf
Rex Bergeron <rexbergeron@comcast.net>
2020-03-03 11:25:08 -0800

    worked on auth for user -anastasia

--- server.js
+++ server.js
@@ -3,12 +3,45 @@ const express = require('express');
 const app = express();
 const request = require('superagent');
 const cors = require('cors');
+const client = require('./lib/client');
 
 
 
 app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
 //fetching database for class of Drinks
 // This searches for all drinks containing specified liquer endpoint.
+const createAuthRoutes = require('./auth/create-auth-routes.js');
+
+const authRoutes = createAuthRoutes({
+    selectUser(email) {
+        return client.query(`
+            SELECT id, email, hash 
+            FROM users
+            WHERE email = $1;
+        `,
+        [email]
+        ).then(result => result.rows[0]);
+    },
+    insertUser(user, hash) {
+        return client.query(`
+            INSERT into users (email, hash)
+            VALUES ($1, $2)
+            RETURNING id, email;
+        `,
+        [user.email, hash]
+        ).then(result => result.rows[0]);
+    }
+});
+
+// before ensure auth, but after other middleware:
+app.use('/auth', authRoutes);
+
+// for every route, on every request, make sure there is a token
+const ensureAuth = require('./lib/auth/ensure-auth');
+
+
 const getVodkaDrinks = async() => {
 
     const drinkApi = await request.get(`https://www.thecocktaildb.com/api/json/v2/${process.env.API_KEY}/filter.php?i=vodka`);
