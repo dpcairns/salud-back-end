ff87c57a909bd986e2a4931606ed0486ed185778
Rex Bergeron <rexbergeron@comcast.net>
2020-03-05 11:57:09 -0800

    hello

--- server.js
+++ server.js
@@ -310,6 +310,7 @@ app.get('/random', async(req, res, next) => {
 
 const getDrinkByName = async(req) => {
     const drinkApi = await request.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${req.params.myCocktail}`);
+    console.log(drinkApi, 'heres the data');
     return drinkApi.body.drinks.map(individualDrink => {
         
         return {
