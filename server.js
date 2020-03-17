3927a0a5ac061626d17a49d640c5aa6ef6fb5600
Danny Cairns <dpcairns@gmail.com>
2020-03-17 16:53:49 -0700

    add feedback

--- server.js
+++ server.js
@@ -106,137 +106,29 @@ app.post('/favorites', async(req, res) => {
     }
 });
 
-const getVodkaDrinks = async() => {
-
-    const drinkApi = await request.get(`https://www.thecocktaildb.com/api/json/v2/${process.env.API_KEY}/filter.php?i=vodka`);
-    return drinkApi.body.drinks.map(vodkaDrink => {
+// all these drink endpoints should be replaced by a single endpoint that takes the drunk type as a parameter
+const getDrinks = async(drink) => {
+    const drinkApi = await request.get(`https://www.thecocktaildb.com/api/json/v2/${process.env.API_KEY}/filter.php?i=${drink}`);
+    return drinkApi.body.drinks.map(someDrink => {
         
         return {
-            name: vodkaDrink.strDrink,
-            image: vodkaDrink.strDrinkThumb,
-            id: vodkaDrink.idDrink  
+            name: someDrink.strDrink,
+            image: someDrink.strDrinkThumb,
+            id: someDrink.idDrink   
         };
     });
 };
 
 
-
-app.get('/vodka', async(req, res, next) => {
+app.get('/drink/:drink', async(req, res, next) => {
     try {
-        const allVodkaDrinks = await getVodkaDrinks();
-        res.json(allVodkaDrinks);
+        const drinks = await getDrinks(req.params.drink);
+        res.json(allDrinks);
     } catch (err) {
         next(err);   
     }
 
-});
-const getTequilaDrinks = async() => {
-    const drinkApi = await request.get(`https://www.thecocktaildb.com/api/json/v2/${process.env.API_KEY}/filter.php?i=tequila`);
-    return drinkApi.body.drinks.map(tequilaDrink => {
-        
-        return {
-            name: tequilaDrink.strDrink,
-            image: tequilaDrink.strDrinkThumb, 
-            id: tequilaDrink.idDrink 
-        };
-    });
-};
 
-app.get('/tequila', async(req, res, next) => {
-    try {
-        const allTequilaDrinks = await getTequilaDrinks();
-        res.json(allTequilaDrinks);
-    } catch (err) {
-        next(err);   
-    }
-
-});
-const getWhiskeyDrinks = async() => {
-    const drinkApi = await request.get(`https://www.thecocktaildb.com/api/json/v2/${process.env.API_KEY}/filter.php?i=whiskey`);
-    return drinkApi.body.drinks.map(whiskeyDrink => {
-        
-        return {
-            name: whiskeyDrink.strDrink,
-            image: whiskeyDrink.strDrinkThumb,
-            id: whiskeyDrink.idDrink   
-        };
-    });
-};
-
-app.get('/whiskey', async(req, res, next) => {
-    try {
-        const allWhiskeyDrinks = await getWhiskeyDrinks();
-        res.json(allWhiskeyDrinks);
-    } catch (err) {
-        next(err);   
-    }
-
-});
-const getRumDrinks = async() => {
-    const drinkApi = await request.get(`https://www.thecocktaildb.com/api/json/v2/${process.env.API_KEY}/filter.php?i=rum`);
-    return drinkApi.body.drinks.map(rumDrink => {
-        
-        return {
-            name: rumDrink.strDrink,
-            image: rumDrink.strDrinkThumb,
-            id: rumDrink.idDrink   
-        };
-    });
-};
-
-app.get('/rum', async(req, res, next) => {
-    try {
-        const allRumDrinks = await getRumDrinks();
-        res.json(allRumDrinks);
-    } catch (err) {
-        next(err);   
-    }
-
-});
-const getGinDrinks = async() => {
-    const drinkApi = await request.get(`https://www.thecocktaildb.com/api/json/v2/${process.env.API_KEY}/filter.php?i=gin`);
-    return drinkApi.body.drinks.map(ginDrink => {
-        
-        return {
-            name: ginDrink.strDrink,
-            image: ginDrink.strDrinkThumb,
-            id: ginDrink.idDrink   
-        };
-    });
-};
-
-app.get('/gin', async(req, res, next) => {
-    try {
-        const allGinDrinks = await getGinDrinks();
-        res.json(allGinDrinks);
-    } catch (err) {
-        next(err);   
-    }
-
-});
-
-
-const getScotchDrinks = async() => {
-    const drinkApi = await request.get(`https://www.thecocktaildb.com/api/json/v2/${process.env.API_KEY}/filter.php?i=scotch`);
-    return drinkApi.body.drinks.map(scotchDrink => {
-        
-        return {
-            name: scotchDrink.strDrink,
-            image: scotchDrink.strDrinkThumb,
-            id: scotchDrink.idDrink   
-        };
-    });
-};
-
-app.get('/scotch', async(req, res, next) => {
-    try {
-        const allScotchDrinks = await getScotchDrinks();
-        res.json(allScotchDrinks);
-    } catch (err) {
-        next(err);   
-    }
-
-});
 const getDrinksId = async(req) => {
     const drinkApi = await request.get(`https://www.thecocktaildb.com/api/json/v2/${process.env.API_KEY}/lookup.php?i=${req.params.myDrinkId}`);
     return drinkApi.body.drinks.map(individualDrink => {
@@ -244,8 +136,12 @@ const getDrinksId = async(req) => {
         return {
             Name: individualDrink.strDrink, 
             Image: individualDrink.strDrinkThumb,
-            Ingredients:[individualDrink.strIngredient1, individualDrink.strIngredient2, individualDrink.strIngredient3, individualDrink.strIngredient4, individualDrink.strIngredient5, individualDrink.strIngredient6, individualDrink.strIngredient7, individualDrink.strIngredient8, individualDrink.strIngredient9, individualDrink.strIngredient10, individualDrink.strIngredient11, individualDrink.strIngredient12, individualDrink.strIngredient13, individualDrink.strIngredient14, individualDrink.strIngredient15],
-            Measurement:[individualDrink.strMeasure1, individualDrink.strMeasure2, individualDrink.strMeasure3, individualDrink.strMeasure4, individualDrink.strMeasure5, individualDrink.strMeasure6, individualDrink.strMeasure7, individualDrink.strMeasure8, individualDrink.strMeasure9, individualDrink.strMeasure10, individualDrink.strMeasure11, individualDrink.strMeasure12, individualDrink.strMeasure13, individualDrink.strMeasure14, individualDrink.strMeasure15],
+            // wow, this api gave you some pretty ugly data to work with, ha :)
+            Ingredients: Object.keys(individualDrink)
+                // filter out the non-ingredient keys
+                .filter(key => !['idDrink', 'strInstructions', 'strDrink', 'strDrinkThumb'].includes(key))
+                // make an array for each of the non-ingredient keys
+                .map(filteredKey => individualDrink[filteredKey]),
             Instructions: individualDrink.strInstructions,
             id: individualDrink.idDrink   
         };
